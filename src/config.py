bfe9f96797746c421f5cfe015bfe301abf2892b1
SiddharthKarmokar <siddkarmokar@gmail.com>
2025-08-18 16:32:14 +0530

    added dummy secrets

--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Secrets(BaseSettings):
-    MONGODB_URL: str
-    DATABASE_NAME: str
-    JWT_HASH_KEY: str
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    UPSTASH_REDIS_REST_URL: str
-    UPSTASH_REDIS_REST_TOKEN: str
-    CELERY_REDIS_URL: str
+    MONGODB_URL: str = "mongodb://localhost:27017"
+    DATABASE_NAME: str = "testdb"
+    JWT_HASH_KEY: str = "test-secret"
+    MAIL_USERNAME: str = "test@example.com"
+    MAIL_PASSWORD: str = "password"
+    UPSTASH_REDIS_REST_URL: str = "http://localhost:6379"
+    UPSTASH_REDIS_REST_TOKEN: str = "dummy"
+    CELERY_REDIS_URL: str = "redis://localhost:6379/0"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    class Config:
+        env_file = ".env"
 
 
 secrets = Secrets()
